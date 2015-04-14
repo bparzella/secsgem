ea7377f009a821e9fffbbf23759bd3ff79ec7464
Benjamin Parzella <bparzella@googlemail.com>
2015-04-14 21:48:16 +0200

    only disconnect servers connection if they are connected

--- secsgem/hsmsConnections.py
+++ secsgem/hsmsConnections.py
@@ -260,7 +260,8 @@ class hsmsMultiServer(_callbackHandler):
 
         if terminateConnections:
             for connection in self.connections:
-                connection.disconnect(separate = True)
+                if connection.connected:
+                    connection.disconnect(separate = True)
 
         self.connectionsLock.release()
 
