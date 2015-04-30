202d6da8cde21f848b3c996559dbab47d314e428
Benjamin Parzella <bparzella@googlemail.com>
2015-05-01 00:02:49 +0200

    some more tests

--- tests/testSecsVariables.py
+++ tests/testSecsVariables.py
@@ -34,7 +34,7 @@ class testSecsVar(unittest.TestCase):
 		self.assertEqual(secsvar.encodeItemHeader(0x10000), "\xB3\x01\x00\x00")
 		self.assertEqual(secsvar.encodeItemHeader(0xFFFFFF), "\xB3\xFF\xFF\xFF")
 
-	def testEncodeItemHeaderTooShort (self):
+	def testEncodeItemHeaderTooShort(self):
 		#dummy object, just to have format code set
 		secsvar = secsVarU4(1337)
 
@@ -47,3 +47,124 @@ class testSecsVar(unittest.TestCase):
 
 		#more than three length bytes worth a value
 		self.assertRaises(ValueError, secsvar.encodeItemHeader, 0x1000000)
+
+	def testDecodeItemHeader(self):
+		#dummy object, just to have format code set
+		secsvar = secsVarU4(1337)
+
+		#two bytes
+		self.assertEqual(secsvar.decodeItemHeader("\xB1\x00")[2], 0)
+		self.assertEqual(secsvar.decodeItemHeader("\xB1\xFF")[2], 0xFF)
+
+		#three bytes
+		self.assertEqual(secsvar.decodeItemHeader("\xB2\x01\x00")[2], 0x100)
+		self.assertEqual(secsvar.decodeItemHeader("\xB2\xFF\xFF")[2], 0xFFFF)
+
+		#four bytes
+		self.assertEqual(secsvar.decodeItemHeader("\xB3\x01\x00\x00")[2], 0x10000)
+		self.assertEqual(secsvar.decodeItemHeader("\xB3\xFF\xFF\xFF")[2], 0xFFFFFF)
+
+	def testDecodeItemHeaderEmpty(self):
+		#dummy object, just to have format code set
+		secsvar = secsVarU4(1337)
+
+		self.assertRaises(ValueError, secsvar.decodeItemHeader, "")
+
+	def testDecodeItemHeaderIllegalPosition(self):
+		#dummy object, just to have format code set
+		secsvar = secsVarU4(1337)
+
+		self.assertRaises(IndexError, secsvar.decodeItemHeader, "\xB1\x00", 10)
+
+	def testDecodeItemHeaderIllegalData(self):
+		#dummy object, just to have format code set
+		secsvar = secsVarU4(1337)
+
+		#two bytes
+		self.assertRaises(ValueError, secsvar.decodeItemHeader, "somerandomdata")
+
+class testSecsVarDynamic(unittest.TestCase):
+	def testConstructorU4(self):
+		secsvar = secsVarDynamic(secsVarU4)
+
+		secsvar.set(10)
+
+		self.assertEqual(10, secsvar.get())
+
+	def testConstructorWrongType(self):
+		secsvar = secsVarDynamic(secsVarU4)
+
+		self.assertRaises(ValueError, secsvar.set, "testString")
+		self.assertRaises(ValueError, secsVarDynamic, secsVarU4, value="testString")
+
+	def testConstructorWrongLengthString(self):
+		secsvar = secsVarDynamic(secsVarString, length=5)
+
+		self.assertRaises(ValueError, secsvar.set, "testString")
+
+	def testConstructorLen(self):
+		secsvar = secsVarDynamic(secsVarString)
+
+		secsvar.set("asdfg")
+
+		self.assertEqual(5, len(secsvar))
+
+	def testConstructorSetGetU4(self):
+		secsvar = secsVarDynamic(secsVarU4)
+
+		secsvar.set(10)
+
+		self.assertEqual(10, secsvar.get())
+
+	def testConstructorSetGetString(self):
+		secsvar = secsVarDynamic(secsVarString)
+
+		secsvar.set("testString")
+
+		self.assertEqual("testString", secsvar.get())
+
+	def testEncodeString(self):
+		secsvar = secsVarDynamic(secsVarString, value="testString")
+
+		self.assertEqual(secsvar.encode(), "A\ntestString")
+
+	def testEncodeU4(self):
+		secsvar = secsVarDynamic(secsVarU4, value=1337)
+
+		self.assertEqual(secsvar.encode(), "\xB1\x04\x00\x00\x059")
+
+	def testDecodeString(self):
+		secsvar = secsVarDynamic(secsVarString)
+
+		secsvar.decode("A\ntestString")
+
+		self.assertEqual(secsvar.get(), "testString")
+
+	def testDecodeU4(self):
+		secsvar = secsVarDynamic(secsVarU4)
+
+		secsvar.decode("\xB1\x04\x00\x00\x059")
+
+		self.assertEqual(secsvar.get(), 1337)
+
+	def testDecodeValueTooLong(self):
+		secsvar = secsVarDynamic(secsVarString, 5)
+
+		self.assertRaises(ValueError, secsvar.decode, ("A\ntestString"))
+
+	def testDecodeEmptyValue(self):
+		secsvar = secsVarDynamic(secsVarString, 5)
+
+		self.assertRaises(ValueError, secsvar.decode, (""))
+
+	def testDecodeItemHeaderIllegalPosition(self):
+		secsvar = secsVarDynamic(secsVarU4, value=1337)
+
+		self.assertRaises(IndexError, secsvar.decode, ("\xB1\x00"), 10)
+
+	def testDecodeItemHeaderIllegalData(self):
+		#dummy object, just to have format code set
+		secsvar = secsVarDynamic(secsVarU4, value=1337)
+
+		#two bytes
+		self.assertRaises(ValueError, secsvar.decode, "somerandomdata")
