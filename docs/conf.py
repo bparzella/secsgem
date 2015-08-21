673ed6bd1ed14327d3c257f8444e0ca7f2377d08
Benjamin Parzella <bparzella@googlemail.com>
2015-08-21 21:28:15 +0200

    searching for rtd "No module named gem.handler" error

--- docs/conf.py
+++ docs/conf.py
@@ -27,10 +27,8 @@ if not on_rtd:  # only import and set the theme if we're building docs locally
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#sys.path.insert(0, os.path.abspath('.'))
-
-sys.path.append(os.path.abspath('../../'))
-sys.path.append(os.path.abspath('../'))
+sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('..'))
 
 # -- General configuration ------------------------------------------------
 
