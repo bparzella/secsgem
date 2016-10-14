53ed346c633bcc1909b6e7c19c64463ca87c65a5
Benjamin Parzella <bparzella@googlemail.com>
2016-10-14 10:51:44 +0200

    changed indents in fysom

--- secsgem/common/fysom.py
+++ secsgem/common/fysom.py
@@ -13,10 +13,10 @@
 # distribute, sublicense, and/or sell copies of the Software, and to
 # permit persons to whom the Software is furnished to do so, subject to
 # the following conditions:
-# 
+#
 # The above copyright notice and this permission notice shall be
 # included in all copies or substantial portions of the Software.
-# 
+#
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
 # EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
 # MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
@@ -205,144 +205,140 @@ __maintainer__ = 'Mansour Behabadi'
 __email__ = 'mansour@oxplot.com'
 
 try:
-  unicode = unicode
+    unicode = unicode
 except NameError:  # pragma: no cover
-  unicode = str
-  basestring = (str, bytes)
+    unicode = str
+    basestring = (str, bytes)
 
 class FysomError(Exception):  # pragma: no cover
-  pass
+    pass
 
 class Fysom(object):  # pragma: no cover
+    def __init__(self, cfg):
+        self._apply(cfg)
 
-  def __init__(self, cfg):
-    self._apply(cfg)
+    def isstate(self, state):
+        return self.current == state
 
-  def isstate(self, state):
-    return self.current == state
+    def can(self, event):
+        return event in self._map and self.current in self._map[event] \
+            and not hasattr(self, 'transition')
 
-  def can(self, event):
-    return event in self._map and self.current in self._map[event] \
-      and not hasattr(self, 'transition')
+    def cannot(self, event):
+        return not self.can(event)
 
-  def cannot(self, event):
-    return not self.can(event)
+    def _apply(self, cfg):
+        init = cfg['initial'] if 'initial' in cfg else None
+        if isinstance(init, basestring):
+            init = {'state': init}
+        events = cfg['events'] if 'events' in cfg else []
+        callbacks = cfg['callbacks'] if 'callbacks' in cfg else {}
+        tmap = {}
+        self._map = tmap
+        self._autoforward = {}
+        if 'autoforward' in cfg:
+            for autoforward in cfg['autoforward']:
+                self._autoforward[autoforward['src']] = autoforward['dst']
 
-  def _apply(self, cfg):
-    init = cfg['initial'] if 'initial' in cfg else None
-    if isinstance(init, basestring):
-      init = {'state': init}
-    events = cfg['events'] if 'events' in cfg else []
-    callbacks = cfg['callbacks'] if 'callbacks' in cfg else {}
-    tmap = {}
-    self._map = tmap
-    self._autoforward = {}
-    if 'autoforward' in cfg:
-      for autoforward in cfg['autoforward']:
-        self._autoforward[autoforward['src']] = autoforward['dst']
+        def add(e):
+            src = [e['src']] if isinstance(e['src'], basestring) else e['src']
+            if e['name'] not in tmap:
+                tmap[e['name']] = {}
+            for s in src:
+                tmap[e['name']][s] = e['dst']
 
-    def add(e):
-      src = [e['src']] if isinstance(e['src'], basestring) else e['src']
-      if e['name'] not in tmap:
-        tmap[e['name']] = {}
-      for s in src:
-        tmap[e['name']][s] = e['dst']
+        if init:
+            if 'event' not in init:
+                init['event'] = 'startup'
+            add({'name': init['event'], 'src': 'none', 'dst': init['state']})
 
-    if init:
-      if 'event' not in init:
-        init['event'] = 'startup'
-      add({'name': init['event'], 'src': 'none', 'dst': init['state']})
+        for e in events:
+            add(e)
 
-    for e in events:
-      add(e)
+        for name in tmap:
+            setattr(self, name, self._build_event(name))
 
-    for name in tmap:
-      setattr(self, name, self._build_event(name))
+        for name in callbacks:
+            setattr(self, name, callbacks[name])
 
-    for name in callbacks:
-      setattr(self, name, callbacks[name])
+        self.current = 'none'
 
-    self.current = 'none'
+        if init and 'defer' not in init:
+            getattr(self, init['event'])()
 
-    if init and 'defer' not in init:
-      getattr(self, init['event'])()
+    def _build_event(self, event):
+        def fn(**kwargs):
+            evt = event
 
-  def _build_event(self, event):
-    def fn(**kwargs):
-      evt = event
+            if hasattr(self, 'transition'):
+                raise FysomError("event %s inappropriate because previous"
+                                 " transition did not complete" % evt)
+            if not self.can(evt):
+                raise FysomError("event %s inappropriate in current state"
+                                 " %s" % (evt, self.current))
+            src = self.current
+            dst = self._map[evt][src]
 
-      if hasattr(self, 'transition'):
-        raise FysomError("event %s inappropriate because previous"
-                         " transition did not complete" % evt)
-      if not self.can(evt):
-        raise FysomError("event %s inappropriate in current state"
-                         " %s" % (evt, self.current))
-      src = self.current
-      dst = self._map[evt][src]
+            transitionAvailable = True
 
-      transitionAvailable = True
+            while transitionAvailable:
+                class _e_obj(object):
+                    pass
+                e = _e_obj()
+                e.fsm, e.event, e.src, e.dst = self, evt, src, dst
+                for k in kwargs:
+                    setattr(e, k, kwargs[k])
 
-      while transitionAvailable:
+                if self.current != dst:
+                    if self._before_event(e) == False:
+                        return
+                    def _tran():
+                        delattr(self, 'transition')
+                        self.current = dst
+                        self._enter_state(e)
+                        self._change_state(e)
+                        self._after_event(e)
+                    self.transition = _tran
 
-        class _e_obj(object):
-          pass
-        e = _e_obj()
-        e.fsm, e.event, e.src, e.dst = self, evt, src, dst
-        for k in kwargs:
-          setattr(e, k, kwargs[k])
+                if self._leave_state(e) != False:
+                    if hasattr(self, 'transition'):
+                        self.transition()
 
-        if self.current != dst:
-          if self._before_event(e) == False:
-            return
-          def _tran():
-            delattr(self, 'transition')
-            self.current = dst
-            self._enter_state(e)
-            self._change_state(e)
-            self._after_event(e)
-          self.transition = _tran
+                if self.current in self._autoforward:
+                    src = dst
+                    dst = self._autoforward[src]
+                    evt = "autoforward" + src + "-" + dst
 
-        if self._leave_state(e) != False:
-          if hasattr(self, 'transition'):
-            self.transition()
+                    transitionAvailable = True
+                else:
+                    transitionAvailable = False
 
-        if self.current in self._autoforward:
-          src = dst
-          dst = self._autoforward[src]
-          evt = "autoforward" + src + "-" + dst
+        return fn
 
-          transitionAvailable = True
-        else:
-          transitionAvailable = False
+    def _before_event(self, e):
+        fnname = 'onbefore' + e.event
+        if hasattr(self, fnname):
+            return getattr(self, fnname)(e)
 
+    def _after_event(self, e):
+        for fnname in ['onafter' + e.event, 'on' + e.event]:
+            if hasattr(self, fnname):
+                return getattr(self, fnname)(e)
 
-      
-    return fn
+    def _leave_state(self, e):
+        fnname = 'onleave' + e.src
+        if hasattr(self, fnname):
+            return getattr(self, fnname)(e)
 
-  def _before_event(self, e):
-    fnname = 'onbefore' + e.event
-    if hasattr(self, fnname):
-      return getattr(self, fnname)(e)
+    def _enter_state(self, e):
+        for fnname in ['onenter' + e.dst, 'on' + e.dst]:
+            if hasattr(self, fnname):
+                return getattr(self, fnname)(e)
 
-  def _after_event(self, e):
-    for fnname in ['onafter' + e.event, 'on' + e.event]:
-      if hasattr(self, fnname):
-        return getattr(self, fnname)(e)
-
-  def _leave_state(self, e):
-    fnname = 'onleave' + e.src
-    if hasattr(self, fnname):
-      return getattr(self, fnname)(e)
-
-  def _enter_state(self, e):
-    for fnname in ['onenter' + e.dst, 'on' + e.dst]:
-      if hasattr(self, fnname):
-        return getattr(self, fnname)(e)
-
-  def _change_state(self, e):
-    fnname = 'onchangestate'
-    if hasattr(self, fnname):
-      return getattr(self, fnname)(e)
+    def _change_state(self, e):
+        fnname = 'onchangestate'
+        if hasattr(self, fnname):
+            return getattr(self, fnname)(e)
 
 if __name__ == '__main__':  # pragma: no cover
-  pass
+    pass
