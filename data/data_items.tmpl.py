251de2aed13f49fa81ef1339f1ac6f70de230b1a
Benjamin Parzella <bparzella@gmail.com>
2023-04-05 12:11:58 +0200

    Add data item generator

---
+++ data/data_items.tmpl.py
@@ -0,0 +1,32 @@
+#####################################################################
+# {{ data.file_name }}
+#
+# (c) Copyright 2021, Benjamin Parzella. All rights reserved.
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This software is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#####################################################################
+"""{{ data.name }} data item."""
+from .. import variables
+from .base import DataItemBase
+
+
+class {{ data.name }}(DataItemBase):
+    """
+    {{ data.description }}.
+
+       :Types: :class:`{{ data.type }} <secsgem.secs.variables.{{ data.type }}>`
+       :Length: {{ data.length }}
+
+{{ data.values }}    """
+
+    __type__ = variables.{{ data.type }}
+    __count__ = {{ data.length }}
+{{ data.extra_variables }}
