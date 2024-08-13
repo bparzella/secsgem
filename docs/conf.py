942c1d662b0feea3095f3b3e238999419c931f22
Benjamin Parzella <bparzella@gmail.com>
2024-08-13 20:57:25 +0200

    Set the readthedocs theme for documentation
    When building the docs on rtd, the theme was not switched, as this was the default theme. The code specific to rtd was removed from the conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -17,18 +17,8 @@ import os
 import os.path
 import pathlib
 
-# on_rtd is whether we are on readthedocs.org, this line of code grabbed from docs.readthedocs.org
-on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+html_theme = 'sphinx_rtd_theme'
 
-if not on_rtd:  # only import and set the theme if we're building docs locally
-    import sphinx_rtd_theme
-    html_theme = 'sphinx_rtd_theme'
-    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-
-# otherwise, readthedocs.org uses their theme by default, so no need to specify it
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath('..'))
 
