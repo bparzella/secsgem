d953073eb9026bfa3bb17cc510002de53cb6c351
Benjamin Parzella <bparzella@gmail.com>
2024-09-08 01:04:21 +0200

    Increase version to 0.3.0-beta.1

--- docs/conf.py
+++ docs/conf.py
@@ -66,7 +66,7 @@ copyright = u'2015-2024, Benjamin Parzella'
 # The short X.Y version.
 version = '0.0'
 # The full version, including alpha/beta/rc tags.
-release = '0.2.0'
+release = '0.3.0-beta.1'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
